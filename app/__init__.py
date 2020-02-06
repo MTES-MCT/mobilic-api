b00bcc613447be896d98810b24efba82bd221889
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-02-06 12:52:46 +0100

    Init migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,12 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+
+Migrate(app, db)
---
+++ config.py
@@ -0,0 +1,3 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/mobilic"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
