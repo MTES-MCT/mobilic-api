ddce0f0c402ac5bc9c73bc7331e0ae317bce3093
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-02-10 22:31:37 +0100

    Relocate api definition to controllers module

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,3 @@
-import graphene
 from flask import Flask
 from flask_graphql import GraphQLView
 from flask_migrate import Migrate
@@ -15,24 +14,9 @@ db = SQLAlchemy(app)
 Migrate(app, db)
 
 
-from app.controllers.activity import ActivityLog
-from app.controllers.user import UserSignup
-from app.controllers.company import CompanySignup
 from app.helpers import cli
+from app.controllers import graphql_schema
 
-from app.helpers.auth import AuthMutation
-
-
-class Mutations(graphene.ObjectType):
-    auth = graphene.Field(
-        AuthMutation, resolver=lambda root, info: AuthMutation()
-    )
-    log_activities = ActivityLog.Field()
-    signup_user = UserSignup.Field()
-    signup_company = CompanySignup.Field()
-
-
-graphql_schema = graphene.Schema(mutation=Mutations)
 
 app.add_url_rule(
     "/api",
--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,1 +1,22 @@
+import graphene
 
+from app.helpers.authentication import AuthMutation
+from app.controllers.activity import ActivityLog
+import app.controllers.user
+import app.controllers.company
+
+
+class Mutations(graphene.ObjectType):
+    auth = graphene.Field(
+        AuthMutation, resolver=lambda root, info: AuthMutation()
+    )
+    log_activities = ActivityLog.Field()
+    signup_user = user.UserSignup.Field()
+    signup_company = company.CompanySignup.Field()
+
+
+class Query(user.Query, company.Query, graphene.ObjectType):
+    pass
+
+
+graphql_schema = graphene.Schema(query=Query, mutation=Mutations)
