5423499b4bf59d002c45ede9ba417507bab3f5c9
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-02-14 01:25:19 +0100

    Modify log activity return type

--- app/data_access/activity.py
+++ app/data_access/activity.py
@@ -1,8 +1,17 @@
 from graphene_sqlalchemy import SQLAlchemyObjectType
+import graphene
 
-from app.models.activity import Activity
+from app.helpers.graphene_types import graphene_enum_type
+from app.models.activity import Activity, ActivityTypes
+
+
+class TeamMateOutput(graphene.ObjectType):
+    id = graphene.Int()
 
 
 class ActivityOutput(SQLAlchemyObjectType):
     class Meta:
         model = Activity
+
+    type = graphene_enum_type(ActivityTypes)()
+    team = graphene.List(graphene.Int)
