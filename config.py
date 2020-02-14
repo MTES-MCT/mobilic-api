6664c57e4718ae57c169359484d554d9c861dc21
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-02-14 02:29:50 +0100

    Use prod config

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
 from datetime import timedelta
+import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/mobilic"
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL", "postgresql://localhost:5432/mobilic"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MINIMUM_ACTIVITY_DURATION = timedelta(minutes=3)
     ACCESS_TOKEN_EXPIRATION = timedelta(minutes=100)
-    JWT_SECRET_KEY = "my-little-secret"
+    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "my-little-secret")
     DISABLE_AUTH_FOR_TESTING = False
 
 
