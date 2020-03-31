9d2dcafcc20ac0282d1e7568f29e5345b931dcc7
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-03-31 20:33:55 +0200

    Add vehicle list to company endpoint

--- app/data_access/company.py
+++ app/data_access/company.py
@@ -5,6 +5,7 @@ from app.domain.permissions import company_admin
 from app.helpers.authorization import with_authorization_policy
 from app.helpers.graphene_types import BaseSQLAlchemyObjectType
 from app.models import Company
+from app.models.vehicle import VehicleOutput
 
 
 class CompanyOutput(BaseSQLAlchemyObjectType):
@@ -13,9 +14,16 @@ class CompanyOutput(BaseSQLAlchemyObjectType):
         only_fields = ("id", "name")
 
     users = graphene.List(UserOutput)
+    vehicles = graphene.List(VehicleOutput)
 
     @with_authorization_policy(
         company_admin, get_target_from_args=lambda self, info: self
     )
     def resolve_users(self, info):
         return self.users
+
+    @with_authorization_policy(
+        company_admin, get_target_from_args=lambda self, info: self
+    )
+    def resolve_vehicles(self, info):
+        return self.vehicles
