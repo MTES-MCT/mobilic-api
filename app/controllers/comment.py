3fdcdd01b04fc956cd68cc58b4fd8463f2e7d9cf
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-04-26 22:50:17 +0200

    Remove event batching

--- app/controllers/comment.py
+++ app/controllers/comment.py
@@ -1,44 +1,29 @@
-from flask_jwt_extended import current_user
+from app.helpers.authentication import current_user
 import graphene
 
 from app import app
-from app.controllers.event import preload_relevant_resources_for_event_logging
 from app.controllers.utils import atomic_transaction
 from app.models.comment import CommentOutput
 from app.domain.log_comments import log_group_comment
 from app.helpers.authorization import with_authorization_policy, authenticated
-from app.models.user import User
 from app.controllers.event import EventInput
 
 
-class CommentInput(EventInput):
-    content = graphene.Field(graphene.String)
-
-
-class CommentLog(graphene.Mutation):
-    class Arguments:
-        data = graphene.List(CommentInput, required=True)
+class LogComment(graphene.Mutation):
+    class Arguments(EventInput):
+        content = graphene.String(required=True)
 
     comments = graphene.List(CommentOutput)
 
     @classmethod
     @with_authorization_policy(authenticated)
-    def mutate(cls, _, info, data):
+    def mutate(cls, _, info, **comment_input):
         with atomic_transaction(commit_at_end=True):
-            app.logger.info(
-                f"Logging comments submitted by {current_user} of company {current_user.company}"
+            app.logger.info(f"Logging comment")
+            log_group_comment(
+                submitter=current_user,
+                content=comment_input["content"],
+                event_time=comment_input["event_time"],
             )
-            events = sorted(data, key=lambda e: e.event_time)
-            preload_relevant_resources_for_event_logging(User.comments)
-            for group_comment in events:
-                log_group_comment(
-                    submitter=current_user,
-                    users=[current_user]
-                    + current_user.acknowledged_team_at(
-                        group_comment.event_time
-                    ),
-                    content=group_comment.content,
-                    event_time=group_comment.event_time,
-                )
 
-        return CommentLog(comments=current_user.comments,)
+        return LogComment(comments=current_user.comments)
--- app/controllers/vehicle_booking.py
+++ app/controllers/vehicle_booking.py
@@ -1,53 +1,62 @@
-from flask_jwt_extended import current_user
+from app.helpers.authentication import current_user
+from sqlalchemy.orm import selectinload
 import graphene
 
 from app import app
-from app.controllers.event import preload_relevant_resources_for_event_logging
 from app.controllers.utils import atomic_transaction
 from app.domain.log_vehicle_booking import log_vehicle_booking
 from app.helpers.authorization import with_authorization_policy, authenticated
 from app.helpers.graphene_types import DateTimeWithTimeStampSerialization
+from app.models import Mission
 from app.models.user import User
 from app.controllers.event import EventInput
 from app.models.vehicle import VehicleOutput
 from app.models.vehicle_booking import VehicleBookingOutput
 
 
+def _preload_db_resources():
+    User.query.options(selectinload(User.submitted_vehicle_bookings)).options(
+        selectinload(User.company)
+    ).filter(User.id == current_user.id).one()
+
+
 class VehicleBookingInput(EventInput):
-    vehicle_id = graphene.Field(graphene.Int, required=False)
-    registration_number = graphene.Field(graphene.String, required=False)
-    user_time = DateTimeWithTimeStampSerialization(required=False)
+    vehicle_id = graphene.Argument(graphene.Int, required=False)
+    mission_id = graphene.Argument(graphene.Int, required=True)
+    registration_number = graphene.Argument(graphene.String, required=False)
+    user_time = graphene.Argument(
+        DateTimeWithTimeStampSerialization, required=False
+    )
 
 
-class VehicleBookingLog(graphene.Mutation):
-    class Arguments:
-        data = graphene.List(VehicleBookingInput, required=True)
+class LogVehicleBooking(graphene.Mutation):
+    Arguments = VehicleBookingInput
 
     vehicle_bookings = graphene.List(VehicleBookingOutput)
     bookable_vehicles = graphene.List(VehicleOutput)
 
     @classmethod
     @with_authorization_policy(authenticated)
-    def mutate(cls, _, info, data):
+    def mutate(cls, _, info, **vehicle_booking_input):
         with atomic_transaction(commit_at_end=True):
             app.logger.info(
-                f"Logging vehicle bookings submitted by {current_user} of company {current_user.company}"
+                f"Logging vehicle booking submitted by {current_user} of company {current_user.company}"
             )
-            events = sorted(data, key=lambda e: e.event_time)
-            preload_relevant_resources_for_event_logging(
-                User.submitted_vehicle_bookings
+            mission = Mission.query.get(vehicle_booking_input["mission_id"])
+            _preload_db_resources()
+            log_vehicle_booking(
+                submitter=current_user,
+                user_time=vehicle_booking_input.get("user_time")
+                or vehicle_booking_input.get("event_time"),
+                mission=mission,
+                event_time=vehicle_booking_input.get("event_time"),
+                vehicle_id=vehicle_booking_input.get("vehicle_id"),
+                registration_number=vehicle_booking_input.get(
+                    "registration_number"
+                ),
             )
-            for vehicle_booking in events:
-                log_vehicle_booking(
-                    submitter=current_user,
-                    user_time=vehicle_booking.user_time
-                    or vehicle_booking.event_time,
-                    event_time=vehicle_booking.event_time,
-                    vehicle_id=vehicle_booking.vehicle_id,
-                    registration_number=vehicle_booking.registration_number,
-                )
 
-        return VehicleBookingLog(
-            vehicle_bookings=current_user.vehicle_bookings,
+        return LogVehicleBooking(
+            vehicle_bookings=mission.vehicle_bookings,
             bookable_vehicles=current_user.bookable_vehicles,
         )
