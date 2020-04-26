ba7a7d31dc1cc2fa2e01e58a3bf854e2f327f3b8
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-04-26 22:46:53 +0200

    Add new endpoints matching new mission logic

--- app/controllers/mission.py
+++ app/controllers/mission.py
@@ -1,45 +1,75 @@
-from flask_jwt_extended import current_user
+from app.helpers.authentication import current_user
 import graphene
+from graphene.types.generic import GenericScalar
 
 from app import app
-from app.controllers.event import preload_relevant_resources_for_event_logging
 from app.controllers.utils import atomic_transaction
-from app.domain.log_missions import log_mission
+from app.domain.log_activities import log_group_activity
+from app.domain.mission import begin_mission
 from app.helpers.authorization import with_authorization_policy, authenticated
-from app.helpers.graphene_types import DateTimeWithTimeStampSerialization
-from app.models.mission import MissionOutput
-from app.models.user import User
+from app.helpers.graphene_types import (
+    DateTimeWithTimeStampSerialization,
+    graphene_enum_type,
+)
+from app.models.activity import InputableActivityType, ActivityType
+from app.models.mission import MissionOutput, Mission
 from app.controllers.event import EventInput
 
 
+class TeamMateInput(graphene.InputObjectType):
+    id = graphene.Int(required=False)
+    first_name = graphene.String(required=False)
+    last_name = graphene.String(required=False)
+
+
 class MissionInput(EventInput):
-    name = graphene.Field(graphene.String)
-    user_time = DateTimeWithTimeStampSerialization(required=False)
+    name = graphene.Argument(graphene.String, required=False)
+    first_activity_type = graphene.Argument(
+        graphene_enum_type(InputableActivityType), required=True
+    )
+    vehicle_registration_number = graphene.String(required=False)
+    vehicle_id = graphene.Int(required=False)
+    team = graphene.List(TeamMateInput, required=False)
 
 
-class MissionLog(graphene.Mutation):
-    class Arguments:
-        data = graphene.List(MissionInput, required=True)
+class BeginMission(graphene.Mutation):
+    Arguments = MissionInput
 
-    missions = graphene.List(MissionOutput)
+    mission = graphene.Field(MissionOutput)
 
     @classmethod
     @with_authorization_policy(authenticated)
-    def mutate(cls, _, info, data):
+    def mutate(cls, _, info, **mission_input):
         with atomic_transaction(commit_at_end=True):
             app.logger.info(
-                f"Logging missions submitted by {current_user} of company {current_user.company}"
+                f"Starting a new mission with name {mission_input.get('name')}"
             )
-            events = sorted(data, key=lambda e: e.event_time)
-            preload_relevant_resources_for_event_logging(
-                User.submitted_missions
-            )
-            for mission in events:
-                log_mission(
-                    submitter=current_user,
-                    user_time=mission.user_time or mission.event_time,
-                    event_time=mission.event_time,
-                    name=mission.name,
-                )
+            mission = begin_mission(user=current_user, **mission_input)
 
-        return MissionLog(missions=current_user.missions)
+        return BeginMission(mission=mission)
+
+
+class EndMission(graphene.Mutation):
+    class Arguments(EventInput):
+        mission_id = graphene.Int(required=True)
+        user_time = DateTimeWithTimeStampSerialization(required=False)
+        expenditures = GenericScalar(required=False)
+
+    mission = graphene.Field(MissionOutput)
+
+    @classmethod
+    @with_authorization_policy(authenticated)
+    def mutate(cls, _, info, **args):
+        with atomic_transaction(commit_at_end=True):
+            mission = Mission.query.get(args["mission_id"])
+            app.logger.info(f"Ending mission {mission}")
+            mission.expenditures = args.get("expenditures")
+            log_group_activity(
+                submitter=current_user,
+                mission=mission,
+                type=ActivityType.REST,
+                event_time=args["event_time"],
+                user_time=args.get("user_time") or args["event_time"],
+            )
+
+        return EndMission(mission=mission)
