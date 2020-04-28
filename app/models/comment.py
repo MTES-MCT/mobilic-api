13daae6a5d57d99b9f6ddd0df7bcc9f6d449fca2
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-04-28 15:11:38 +0200

    Associate comment to a mission and not to the user anymore

--- app/models/comment.py
+++ app/models/comment.py
@@ -1,15 +1,22 @@
+from sqlalchemy.orm import backref
+
 from app import db
 from app.helpers.graphene_types import (
     BaseSQLAlchemyObjectType,
     graphene_enum_type,
 )
-from app.models.event import UserEventBaseModel, Dismissable, DismissType
+from app.models.event import EventBaseModel, Dismissable, DismissType
 
 
-class Comment(UserEventBaseModel, Dismissable):
+class Comment(EventBaseModel, Dismissable):
     backref_base_name = "comments"
 
     content = db.Column(db.TEXT, nullable=False)
+
+    mission_id = db.Column(
+        db.Integer, db.ForeignKey("mission.id"), index=True, nullable=False
+    )
+    mission = db.relationship("Mission", backref=backref("comments"))
 
 
 class CommentOutput(BaseSQLAlchemyObjectType):
