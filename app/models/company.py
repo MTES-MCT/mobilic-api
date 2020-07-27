729b8b83b5c4da2a9113abc913324833826eaa8c
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-07-27 23:14:30 +0200

    Refactor company model to include SIREN API data

--- app/models/company.py
+++ app/models/company.py
@@ -1,9 +1,45 @@
+from datetime import date
+from sqlalchemy.dialects.postgresql import JSONB
+
 from app.models.base import BaseModel
 from app import db
 
 
 class Company(BaseModel):
-    name = db.Column(db.String(255), nullable=False)
+    usual_name = db.Column(db.String(255), nullable=False)
+
+    siren = db.Column(db.Integer, unique=True, nullable=False)
+
+    sirets = db.Column(db.ARRAY(db.String(14)), nullable=True)
+
+    siren_api_info = db.Column(JSONB(none_as_null=True), nullable=True)
+
+    @property
+    def name(self):
+        return self.usual_name
 
     def __repr__(self):
         return f"<Company [{self.id}] : {self.name}>"
+
+    @property
+    def users(self):
+        return [
+            e.user
+            for e in self.employments
+            if e.is_acknowledged
+            and e.start_date
+            <= date.today()
+            <= (e.end_date or date(2100, 1, 1))
+        ]
+
+    @property
+    def workers(self):
+        return [
+            e.user
+            for e in self.employments
+            if e.is_acknowledged
+            and e.start_date
+            <= date.today()
+            <= (e.end_date or date(2100, 1, 1))
+            and not e.has_admin_rights
+        ]
