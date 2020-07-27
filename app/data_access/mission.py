7b5e9dea93a37edddceafa0b38d9f35fc3c63e42
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-07-27 23:04:04 +0200

    Remove obsolete team changes

--- app/data_access/mission.py
+++ app/data_access/mission.py
@@ -14,7 +14,6 @@ class MissionOutput(BaseSQLAlchemyObjectType):
     activities = graphene.List(ActivityOutput)
     expenditures = graphene.List(ExpenditureOutput)
     validated = graphene.Field(graphene.Boolean)
-    team_changes = graphene.List(lambda: TeamChange)
 
     def resolve_activities(self, info):
         return self.acknowledged_activities
@@ -25,14 +24,3 @@ class MissionOutput(BaseSQLAlchemyObjectType):
     def resolve_validated(self, info):
         user = getattr(info.context, "user_being_queried", current_user)
         return self.validated_by(user)
-
-    def resolve_team_changes(self, info):
-        return [
-            {**tc, "mission_id": self.id}
-            for user_team_changes in self.team_mate_status_history().values()
-            for tc in user_team_changes
-            if tc["coworker"] != current_user
-        ]
-
-
-from app.data_access.team_change import TeamChange
