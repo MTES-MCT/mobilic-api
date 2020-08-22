d94d62134bc4179b4530abdb6752811a060148a7
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2020-08-22 13:20:53 +0200

    Compute csrf state directly in the API

--- app/controllers/user.py
+++ app/controllers/user.py
@@ -1,5 +1,7 @@
 import graphene
 from flask import redirect, request
+from uuid import uuid4
+from urllib.parse import quote, urlencode
 
 from app.controllers.utils import atomic_transaction
 from app.data_access.user import UserOutput
@@ -81,16 +83,25 @@ class CreateUserLogin(graphene.Mutation):
 
 @app.route("/fc/authorize")
 def redirect_to_fc_authorize():
+    query_params = {
+        "state": uuid4().hex,
+        "nonce": uuid4().hex,
+        "response_type": "code",
+        "scope": "openid given_name family_name preferred_username birthdate",
+        "client_id": app.config["FC_CLIENT_ID"],
+        "acr_values": "eidas3",
+    }
     return redirect(
-        f"{app.config['FC_URL']}/api/v1/authorize?{request.query_string.decode('utf-8')}",
+        f"{app.config['FC_URL']}/api/v1/authorize?{request.query_string.decode('utf-8')}&{urlencode(query_params, quote_via=quote)}",
         code=308,
     )
 
 
 @app.route("/fc/logout")
 def redirect_to_fc_logout():
+    query_params = {"state": uuid4().hex}
     return redirect(
-        f"{app.config['FC_URL']}/api/v1/logout?{request.query_string.decode('utf-8')}",
+        f"{app.config['FC_URL']}/api/v1/logout?{request.query_string.decode('utf-8')}&{urlencode(query_params, quote_via=quote)}",
         code=308,
     )
 
@@ -101,6 +112,7 @@ class FranceConnectLogin(graphene.Mutation):
         invite_token = graphene.String(
             required=False, description="Lien d'invitation"
         )
+        state = graphene.String(required=True)
         original_redirect_uri = graphene.String(required=True)
         create = graphene.Boolean(required=False)
 
@@ -113,6 +125,7 @@ class FranceConnectLogin(graphene.Mutation):
         info,
         authorization_code,
         original_redirect_uri,
+        state,
         invite_token=None,
         create=False,
     ):
