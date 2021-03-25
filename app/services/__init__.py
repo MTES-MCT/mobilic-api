bcbfc302a525ebb902673b6233df63c1853bdc4e
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2021-03-25 16:56:39 +0100

    Fix service

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -7,10 +7,7 @@ def service_decorator(f):
     def wrapper(*args, **kwargs):
         from app import app
 
-        token = request.form.get("token")
-        print(request.form)
-        print(request.data)
-        app.logger.info(f"Token : {token}")
+        token = request.data.decode("utf-8")
         if token and token == app.config["MOBILIC_SERVICE_ACTOR_TOKEN"]:
             return f(*args, **kwargs)
         abort(403 if token else 401)
