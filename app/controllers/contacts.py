d0838b1e94c00462dda8f5b8f5c27d81475dfc05
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2021-09-07 14:53:45 +0200

    Add all mailing lists for NL

--- app/controllers/contacts.py
+++ app/controllers/contacts.py
@@ -1,7 +1,8 @@
 from flask import jsonify
 from webargs import fields
 from flask_apispec import use_kwargs
-from marshmallow import Schema
+from marshmallow import Schema, validate
+from enum import Enum
 
 from app import app, mailer
 from app.helpers.authentication import (
@@ -9,24 +10,36 @@ from app.helpers.authentication import (
     current_user,
     require_auth,
 )
-from app.helpers.mail import MailjetError
+from app.helpers.mail import MailjetError, MailjetContactLists
 from app.helpers.validation import (
     validate_clean_email_string,
     clean_email_string,
 )
 
 
-class EmailSchema(Schema):
+# Careful when modifying this : the keys represent the possible values that are expected by the subscription endpoint
+class SubscriptionMapEnum(int, Enum):
+    EMPLOYEE = MailjetContactLists.NL_EMPLOYEES
+    ADMIN = MailjetContactLists.NL_ADMINS
+    CONTROLLER = MailjetContactLists.NL_CONTROLLERS
+    SOFTWARE = MailjetContactLists.NL_SOFTWARES
+
+
+class NLSubscriptionSchema(Schema):
     email = fields.String(
         required=False,
         validate=lambda s: validate_clean_email_string(clean_email_string(s)),
     )
+    type = fields.String(
+        required=True,
+        validate=validate.OneOf(list(SubscriptionMapEnum.__members__.keys())),
+    )
 
 
 @app.route("/contacts/subscribe_to_newsletter", methods=["POST"])
-@use_kwargs(EmailSchema(), apply=True)
+@use_kwargs(NLSubscriptionSchema(), apply=True)
 @optional_auth
-def subscribe_to_newsletter(email=None):
+def subscribe_to_newsletter(type, email=None):
     if not current_user and not email:
         return (
             jsonify(
@@ -38,7 +51,9 @@ def subscribe_to_newsletter(email=None):
         )
     actual_email = current_user.email if current_user else email
     try:
-        mailer.subscribe_email_to_contact_list(actual_email)
+        mailer.subscribe_email_to_contact_list(
+            actual_email, SubscriptionMapEnum.__members__[type].value
+        )
     except MailjetError as e:
         return jsonify({"error": e.message}), 500
     return jsonify({"success": True}), 200
