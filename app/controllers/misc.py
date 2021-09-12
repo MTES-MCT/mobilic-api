e760b1ec4c91fc499edb75bcedd8d6f575224e3e
Rayann Hamdan <rayann.hamdan@hotmail.fr>
2021-09-12 22:07:49 +0200

    Cache results from Livestorm API

--- app/controllers/misc.py
+++ app/controllers/misc.py
@@ -1,10 +1,14 @@
 from flask import jsonify
+from cachetools import cached, TTLCache
 
 from app import app
 from app.helpers.livestorm import livestorm, NoLivestormCredentialsError
 
+ttl_cache = TTLCache(maxsize=1, ttl=3600)
+
 
 @app.route("/next-webinars", methods=["GET"])
+@cached(cache=ttl_cache)
 def get_webinars_list():
     if not app.config["LIVESTORM_API_TOKEN"]:
         raise NoLivestormCredentialsError()
