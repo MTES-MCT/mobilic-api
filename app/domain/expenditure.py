e958b98f513a57d7afd880f39b2f7302f78382c2
Raphaël Taïeb <raphael.taieb@beta.gouv.fr>
2021-10-04 15:29:47 +0200

    PR comments

--- app/domain/expenditure.py
+++ app/domain/expenditure.py
@@ -1,7 +1,21 @@
 from app import db
 from app.domain.permissions import check_actor_can_log_on_mission_for_user_at
+from app.helpers.errors import ExpenditureDateNotIncludedInMissionRangeError
 from app.models.expenditure import Expenditure
 from app.helpers.authorization import AuthorizationError
+
+
+def check_date_is_during_mission(spending_date, mission, user):
+    mission_activities = mission.activities_for(user)
+
+    if len(mission_activities) == 0:
+        raise ExpenditureDateNotIncludedInMissionRangeError()
+
+    mission_start = mission_activities[0].start_time.date()
+    mission_end = mission_activities[-1].end_time.date()
+
+    if spending_date < mission_start or spending_date > mission_end:
+        raise ExpenditureDateNotIncludedInMissionRangeError()
 
 
 def log_expenditure(
@@ -16,7 +30,10 @@ def log_expenditure(
             "Expenditures have been disabled for this company"
         )
 
-    # 2. Log the expenditure
+    # 2. Consistency check
+    check_date_is_during_mission(spending_date, mission, user)
+
+    # 3. Log the expenditure
     expenditure = Expenditure(
         type=type,
         reception_time=reception_time,
