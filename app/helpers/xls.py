27954e6a8920a835176a9b8222744f319ef388d4
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2022-05-04 13:59:39 +0200

    Invert company and date lines in header

--- app/helpers/xls.py
+++ app/helpers/xls.py
@@ -637,15 +637,15 @@ def write_work_days_sheet_header(wb, sheet, companies, max_date, min_date):
     sheet.write(
         0,
         0,
-        "Date des données exportées : du {0} au {1}".format(
-            min_date.strftime("%d/%m/%Y"), max_date.strftime("%d/%m/%Y")
-        ),
+        "Entreprise : {0}".format(", ".join(c.name for c in companies)),
         wb.add_format({"bold": True}),
     )
     sheet.write(
         1,
         0,
-        "Entreprise : {0}".format(", ".join(c.name for c in companies)),
+        "Date des données exportées : du {0} au {1}".format(
+            min_date.strftime("%d/%m/%Y"), max_date.strftime("%d/%m/%Y")
+        ),
         wb.add_format({"bold": True}),
     )
     sheet.write(
