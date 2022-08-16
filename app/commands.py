80dab1da8bd16fb6dd01bd7a7d002ea45c4e9004
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2022-08-16 20:44:40 +0200

    feat: add cli to init regulation alerts

--- app/commands.py
+++ app/commands.py
@@ -2,12 +2,14 @@ import os
 import sys
 from unittest import TestLoader, TextTestRunner
 
-from app import app
+from config import TestConfig
 
+from app import app
+from app.domain.regulations import compute_regulations
+from app.helpers.submitter_type import SubmitterType
+from app.models.mission_validation import MissionValidation
 from app.seed import clean as seed_clean
 from app.seed import seed as seed_seed
-
-from config import TestConfig
 
 
 @app.cli.command(with_appcontext=False)
@@ -35,3 +37,28 @@ def clean():
 def seed():
     """Inject tests data in database."""
     seed_seed()
+
+
+@app.cli.command("init_regulation_alerts", with_appcontext=True)
+def init_regulation_alerts():
+    """Initialize alerts for all validated missions"""
+    validated_missions = MissionValidation.query.all()
+    for validated_mission in validated_missions:
+        mission_activities = validated_mission.mission.acknowledged_activities
+        if mission_activities:
+            mission_start = mission_activities[0].start_time.date()
+            mission_end = (
+                mission_activities[-1].end_time.date()
+                if mission_activities[-1].end_time
+                else None
+            )
+            submitter_type = (
+                SubmitterType.ADMIN
+                if validated_mission.is_admin
+                else SubmitterType.EMPLOYEE
+            )
+            users = set([a.user for a in mission_activities])
+            for u in users:
+                compute_regulations(
+                    u, mission_start, mission_end, submitter_type
+                )
