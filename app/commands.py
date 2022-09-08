eb0780225ca78277147b02c99e3e7efa01494341
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2022-09-08 13:58:55 +0200

    feat(init): split process in parts

--- app/commands.py
+++ app/commands.py
@@ -2,14 +2,15 @@ import os
 import sys
 from unittest import TestLoader, TextTestRunner
 
+import click
 import progressbar
-from app.models.user import User
 from config import TestConfig
 
 from app import app
 from app.controllers.utils import atomic_transaction
 from app.domain.regulations import compute_regulation_for_user
 from app.models.mission_validation import MissionValidation
+from app.models.user import User
 from app.seed import clean as seed_clean
 from app.seed import seed as seed_seed
 
@@ -42,11 +43,25 @@ def seed():
 
 
 @app.cli.command("init_regulation_alerts", with_appcontext=True)
-def init_regulation_alerts():
-    """Initialize alerts for all users"""
+@click.argument("part", type=click.INT)
+def init_regulation_alerts(part):
+    """
+    Initialize alerts for users from part PART
+
+    PART is a number between 1 and 4.
+    It is used to split all users in 4 parts using modulo on user_id.
+    """
+
+    nb_parts = 4
+    if part < 1 or part > nb_parts:
+        click.echo(f"ERROR: [part] should be between 1 and {nb_parts}")
+        sys.exit(1)
+
+    print(f"Computing regulation alerts ({part}/{nb_parts})")
     widgets = [progressbar.Percentage(), progressbar.Bar()]
-    users = User.query.all()
+    users = User.query.filter(User.id % nb_parts == part - 1).all()
     max_value = len(users) if users else 0
+    print(f"{max_value} users to process")
     bar = progressbar.ProgressBar(widgets=widgets, max_value=max_value).start()
     i = 0
     for user in users:
