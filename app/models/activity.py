0e6ff2fbc7aa60b6eccdc4e16897dfe719e3e3dc
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2022-10-31 19:19:59 +0100

    fix: update last_update_time when freezing activity

--- app/models/activity.py
+++ app/models/activity.py
@@ -115,7 +115,11 @@ class Activity(UserEventBaseModel, Dismissable, Period):
             if frozen_version.end_time:
                 self.end_time = frozen_version.end_time
             else:
+                # 1mn added to comply with "activity_start_time_before_end_time" constraint
                 self.end_time = at_time + timedelta(minutes=1)
+                # last_update_time updated to comply with "activity_end_time_before_update_time" constraint
+                self.last_update_time = at_time
+                # these changes will not be commited see app/controllers/controller.py
             return self
         else:
             return None
