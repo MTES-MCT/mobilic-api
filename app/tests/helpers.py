bbd30f2ffee1dacfc0d363f809b917c78e13ae26
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2022-11-03 17:59:27 +0100

    test: add test to get alerts

--- app/tests/helpers.py
+++ app/tests/helpers.py
@@ -1,17 +1,13 @@
-from collections import namedtuple
-from unittest.mock import patch, MagicMock
-
-from flask.testing import FlaskClient
-from freezegun import freeze_time
-from datetime import datetime
-from contextlib import contextmanager
-
 import re
+from collections import namedtuple
+from contextlib import contextmanager
+from datetime import datetime
 
-from app import app, db
+from app import db
 from app.helpers.time import to_timestamp
-from app.models import User, ControllerUser
+from app.models import ControllerUser, User
 from app.tests import test_post_graphql, test_post_graphql_unexposed
+from freezegun import freeze_time
 
 DBEntryUpdate = namedtuple("DBUnitUpdate", ["model", "before", "after"])
 MatchingExpectedChangesWithDbDiff = namedtuple(
@@ -228,6 +224,25 @@ class ApiRequests:
           }
         }
       }
+    """
+
+    get_alerts = """
+        query getAlerts($userId: Int!, $day: Date!, $submitterType: SubmitterTypeEnum!, $unit: UnitTypeEnum) {
+            regulationComputation(userId: $userId, day: $day, submitterType: $submitterType) {
+              userId,
+              day,
+              submitterType,
+              checks(unit: $unit) {
+                id,
+                type,
+                unit,
+                regulationRule,
+                alerts(userId: $userId, day: $day, submitterType: $submitterType) {
+                  extra,
+                }
+              }
+            }
+        }
     """
 
 
---
+++ app/tests/test_get_alerts.py
@@ -0,0 +1,81 @@
+from app.helpers.submitter_type import SubmitterType
+from app.models.regulation_check import RegulationCheck
+from app.seed import CompanyFactory, UserFactory
+from app.seed.factories import (
+    EmploymentFactory,
+    RegulationComputationFactory,
+    RegulatoryAlertFactory,
+)
+from app.seed.helpers import get_date
+from app.tests import BaseTest, test_post_graphql
+from app.tests.helpers import ApiRequests
+
+ADMIN_EMAIL = "admin@email.com"
+EMPLOYEE_EMAIL = "employee@email.com"
+
+
+class TestGetAlerts(BaseTest):
+    def setUp(self):
+        super().setUp()
+
+        company = CompanyFactory.create(
+            usual_name="Company Name", siren="1122334", allow_transfers=True
+        )
+
+        admin = UserFactory.create(
+            email=ADMIN_EMAIL,
+            password="password",
+            first_name="Admin",
+            last_name="Admin",
+        )
+        EmploymentFactory.create(
+            company=company, submitter=admin, user=admin, has_admin_rights=True
+        )
+
+        employee = UserFactory.create(
+            email=EMPLOYEE_EMAIL,
+            password="password",
+        )
+        EmploymentFactory.create(
+            company=company,
+            submitter=admin,
+            user=employee,
+            has_admin_rights=False,
+        )
+
+        RegulationComputationFactory.create(
+            day=get_date(how_many_days_ago=2),
+            submitter_type=SubmitterType.EMPLOYEE,
+            user=employee,
+        )
+
+        RegulationComputationFactory.create(
+            day=get_date(how_many_days_ago=1),
+            submitter_type=SubmitterType.EMPLOYEE,
+            user=employee,
+        )
+
+        regulation_check = RegulationCheck.query.first()
+        RegulatoryAlertFactory.create(
+            day=get_date(how_many_days_ago=1),
+            submitter_type=SubmitterType.EMPLOYEE,
+            user=employee,
+            regulation_check=regulation_check,
+        )
+
+        self.company = company
+        self.admin = admin
+        self.employee = employee
+
+    def test_user_can_access_his_own_alerts(self):
+        response = test_post_graphql(
+            ApiRequests.get_alerts,
+            mock_authentication_with_user=self.employee,
+            variables=dict(
+                userId=self.employee.id,
+                day=get_date(how_many_days_ago=1).strftime("%Y-%m-%d"),
+                submitterType=SubmitterType.EMPLOYEE,
+            ),
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNone(response.json.get("errors"))
