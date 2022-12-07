8e8ff7ee47be2e968b19f1dc2680bce58f86483f
Tristan Gueguen <tristan.gueguen@gmail.com>
2022-12-07 11:57:06 +0100

    renamed input variable

--- app/tests/test_helpers.py
+++ app/tests/test_helpers.py
@@ -6,37 +6,37 @@ from app.helpers.time import get_uninterrupted_datetime_ranges
 
 class TestHelper(TestCase):
     def test_datetime_ranges_simple(self):
-        input = [
+        dates = [
             datetime(2022, 1, 1),
             datetime(2022, 1, 3),
             datetime(2022, 1, 2),
         ]
 
-        result = get_uninterrupted_datetime_ranges(input)
+        result = get_uninterrupted_datetime_ranges(dates)
         self.assertEquals(1, len(result))
         self.assertEqual(datetime(2022, 1, 1), result[0][0])
         self.assertEqual(datetime(2022, 1, 3), result[0][1])
 
     def test_datetime_ranges_empty(self):
-        input = []
+        dates = []
 
-        result = get_uninterrupted_datetime_ranges(input)
+        result = get_uninterrupted_datetime_ranges(dates)
 
         self.assertEquals(0, len(result))
 
     def test_datetime_ranges_one_date(self):
-        input = [
+        dates = [
             datetime(2022, 1, 1),
         ]
 
-        result = get_uninterrupted_datetime_ranges(input)
+        result = get_uninterrupted_datetime_ranges(dates)
 
         self.assertEquals(1, len(result))
         self.assertEqual(datetime(2022, 1, 1), result[0][0])
         self.assertEqual(datetime(2022, 1, 1), result[0][1])
 
     def test_datetime_ranges_simple(self):
-        input = [
+        dates = [
             datetime(2022, 1, 1),
             datetime(2022, 1, 3),
             datetime(2022, 1, 2),
@@ -44,7 +44,7 @@ class TestHelper(TestCase):
             datetime(2022, 1, 7),
         ]
 
-        result = get_uninterrupted_datetime_ranges(input)
+        result = get_uninterrupted_datetime_ranges(dates)
         self.assertEquals(2, len(result))
         self.assertEqual(datetime(2022, 1, 1), result[0][0])
         self.assertEqual(datetime(2022, 1, 3), result[0][1])
