97a9a0d2dff6e8975158fced081aaa47fddc9357
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2023-01-03 12:11:50 +0100

    Update migration script down revision

--- migrations/versions/80dfc91bbd27_third_party_client_employment_.py
+++ migrations/versions/80dfc91bbd27_third_party_client_employment_.py
@@ -1,7 +1,7 @@
 """third_party_client_employment.invitation_token can be null
 
 Revision ID: 80dfc91bbd27
-Revises: b5afcd504879
+Revises: 2b887f9b571b
 Create Date: 2022-12-28 16:28:32.007804
 
 """
@@ -9,7 +9,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "80dfc91bbd27"
-down_revision = "b5afcd504879"
+down_revision = "2b887f9b571b"
 branch_labels = None
 depends_on = None
 
