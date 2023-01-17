875ec945712f2879cdc0fa887c99ca2d82a8894d
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2023-01-17 16:27:24 +0100

    Formatting

--- app/controllers/third_party_employment.py
+++ app/controllers/third_party_employment.py
@@ -130,8 +130,11 @@ class Query(graphene.ObjectType):
             ~ThirdPartyClientEmployment.is_dismissed,
         ).one_or_none()
 
-        if client_employment_link and not check_protected_client_id_company_id(
-            client_employment_link.employment.company_id
+        if (
+            client_employment_link
+            and not check_protected_client_id_company_id(
+                client_employment_link.employment.company_id
+            )
         ):
             raise AuthenticationError("Company token has been revoked")
 
