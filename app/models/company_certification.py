0ea4adc226a08ff1fcd6859b26ffec2bdd350a12
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2023-03-27 12:56:54 +0200

    feat: add property certified

--- app/models/company_certification.py
+++ app/models/company_certification.py
@@ -18,3 +18,13 @@ class CompanyCertification(BaseModel):
     not_too_many_changes = db.Column(db.Boolean, default=False, nullable=False)
     validate_regularly = db.Column(db.Boolean, default=False, nullable=False)
     log_in_real_time = db.Column(db.Boolean, default=False, nullable=False)
+
+    @property
+    def certified(self):
+        return (
+            self.be_active
+            and self.be_compliant
+            and self.not_too_many_changes
+            and self.validate_regularly
+            and self.log_in_real_time
+        )
