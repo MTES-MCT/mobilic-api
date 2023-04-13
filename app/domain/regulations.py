a53bbd99622d56b851977d5fa96440fc81afe986
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2023-04-13 14:41:12 +0200

    fix(init alerts): clean all existing alerts

--- app/domain/regulations.py
+++ app/domain/regulations.py
@@ -194,53 +194,22 @@ def compute_weekly_rest_duration(week, tz):
 
 
 def compute_regulation_for_user(user):
-    # on pourrait ajouter un PERIOD_START et un period_END
-
     #####
     # CLEAN previous data
     # This is mainly done to remove wrongly computed data
-    # This could be optional ?
     ####
 
-    # Determine period start and end to clear previous alerts
-    first_user_activity = user.first_activity_after(None)
-
-    if not first_user_activity:
-        return
-
-    today = date.today()
-    last_user_activity = user.latest_activity_before(to_datetime(today))
-    period_start = first_user_activity.start_time.date()
-    period_end = (
-        last_user_activity.end_time.date()
-        if last_user_activity.end_time
-        else today
-    )
-    period_start = period_start - timedelta(days=1)
-    week_period_start = get_first_day_of_week(period_start)
-    week_period_end = get_last_day_of_week(period_end)
-
-    # Maybe we flagged some day as Computed by mistake before ?
     db.session.query(RegulationComputation).filter(
         RegulationComputation.user == user,
-        RegulationComputation.day >= period_start,
-        RegulationComputation.day <= period_end,
     ).delete(synchronize_session=False)
 
-    # If we don't recompute on same time periods later, some alerts wouldn't be removed
-    for submitter_type in [SubmitterType.ADMIN, SubmitterType.EMPLOYEE]:
-        clean_current_alerts(
-            user,
-            period_start,
-            period_end,
-            week_period_start,
-            week_period_end,
-            submitter_type,
-        )
+    db.session.query(RegulatoryAlert).filter(
+        RegulatoryAlert.user == user,
+    ).delete(synchronize_session=False)
     ######
 
     #####
-    # Compute alerts
+    # COMPUTE alerts
     #####
     for submitter_type in [SubmitterType.ADMIN, SubmitterType.EMPLOYEE]:
         (work_days, _) = group_user_events_by_day_with_limit(
