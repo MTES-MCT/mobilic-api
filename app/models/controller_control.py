885c6f089b9282f12c7553379fccf40ed13e69f4
Tristan Gueguen <tristan.gueguen@gmail.com>
2023-06-06 11:50:15 +0200

    migration and field

--- app/models/controller_control.py
+++ app/models/controller_control.py
@@ -46,6 +46,9 @@ class ControllerControl(BaseModel, RandomNineIntId):
     control_bulletin_creation_time = db.Column(
         DateTimeStoredAsUTC, nullable=True
     )
+    control_bulletin_first_download_time = db.Column(
+        DateTimeStoredAsUTC, nullable=True
+    )
 
     @property
     def history_end_date(self):
