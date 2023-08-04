678d6fe145f32be750436f50f157a8e1a782041f
Tristan Gueguen <tristan.gueguen@gmail.com>
2023-08-04 16:13:17 +0200

    report infractions when creating a control

--- app/models/controller_control.py
+++ app/models/controller_control.py
@@ -6,6 +6,7 @@ from sqlalchemy import Enum
 from sqlalchemy.dialects.postgresql import JSONB
 
 from app import db, app
+from app.domain.regulation_computations import get_regulatory_alerts
 from app.domain.work_days import group_user_events_by_day_with_limit
 from app.helpers.db import DateTimeStoredAsUTC
 from app.models import User
@@ -69,6 +70,29 @@ class ControllerControl(BaseModel, RandomNineIntId):
         return (
             f"{self.id}-{today.strftime('%Y')}-{self.controller_user.greco_id}"
         )
+
+    def report_infractions(self):
+        regulatory_alerts = get_regulatory_alerts(
+            user_id=self.user.id,
+            start_date=self.history_start_date,
+            end_date=self.history_end_date,
+        )
+        reported_infractions = []
+        for regulatory_alert in regulatory_alerts:
+            extra = json.loads(regulatory_alert.extra)
+            if not extra or not "sanction_code" in extra:
+                continue
+            sanction_code = extra.get("sanction_code")
+            if "NATINF" not in sanction_code:
+                continue
+            reported_infractions.append(
+                {
+                    "sanction": extra.get("sanction_code"),
+                    "date": regulatory_alert.day.isoformat(),
+                }
+            )
+        self.reported_infractions = reported_infractions
+        db.session.commit()
 
     @staticmethod
     def create_no_lic_control(controller_id):
@@ -159,4 +183,6 @@ class ControllerControl(BaseModel, RandomNineIntId):
             )
             db.session.add(new_control)
             db.session.commit()
+
+            new_control.report_infractions()
             return new_control
