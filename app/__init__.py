53bc3d18afb41c02ed0e0f0ea574763ac0590869
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2023-08-09 17:57:06 +0200

    Init sentry monitoring

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,29 @@
+import os
+
+import sentry_sdk
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
 from flask import Flask
 from flask_apispec import FlaskApiSpec
 from flask_compress import Compress
-from flask_migrate import Migrate
 from flask_cors import CORS
+from flask_migrate import Migrate
 from werkzeug.exceptions import HTTPException
-from apispec import APISpec
-from apispec.ext.marshmallow import MarshmallowPlugin
 
 import config
-from config import MOBILIC_ENV
 from app.helpers.db import SQLAlchemyWithStrongRefSession
 from app.helpers.errors import MobilicError
-from app.helpers.siren import SirenAPIClient
 from app.helpers.request_parser import CustomRequestParser
+from app.helpers.siren import SirenAPIClient
 from app.templates.filters import JINJA_CUSTOM_FILTERS
-from elasticapm.contrib.flask import ElasticAPM
-import logging
+from config import MOBILIC_ENV
 
-
+sentry_sdk.init(
+    dsn=os.environ.get("SENTRY_DSN"),
+    traces_sample_rate=os.environ.get("SENTRY_SAMPLE_RATE"),
+)
 app = Flask(__name__)
+
 Compress(app)
 # See list of possible settings at https://pypi.org/project/Flask-Compress/1.13/
 app.config.update({"COMPRESS_MIN_SIZE": 100})
@@ -38,7 +43,6 @@ docs = FlaskApiSpec(app)
 
 app.config.from_object(getattr(config, f"{MOBILIC_ENV.capitalize()}Config"))
 
-apm = ElasticAPM(app, logging=logging.ERROR)
 siren_api_client = SirenAPIClient(app.config["SIREN_API_KEY"])
 
 for name, filter in JINJA_CUSTOM_FILTERS.items():
