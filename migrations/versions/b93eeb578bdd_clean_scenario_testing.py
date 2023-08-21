159380df79c291ac2232fc2ef11fd1cb539466ac
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2023-08-21 17:43:53 +0200

    clean: remove data from scenario testing

---
+++ migrations/versions/b93eeb578bdd_clean_scenario_testing.py
@@ -0,0 +1,21 @@
+"""clean_scenario_testing
+
+Revision ID: b93eeb578bdd
+Revises: 096862334173
+Create Date: 2023-08-21 14:07:40.391195
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "b93eeb578bdd"
+down_revision = "096862334173"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("DELETE FROM scenario_testing")
+    op.execute("REINDEX TABLE scenario_testing")
