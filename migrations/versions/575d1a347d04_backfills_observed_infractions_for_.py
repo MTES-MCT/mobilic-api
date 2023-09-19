329a228a138eaa26c5a89d0cbbf30ced7a67abe7
Gaspard Lonchampt <gaspard.lonchampt@beta.gouv.fr>
2023-09-19 17:59:05 +0200

    MEP: 2nd release

---
+++ migrations/versions/575d1a347d04_backfills_observed_infractions_for_.py
@@ -0,0 +1,43 @@
+"""backfills observed_infractions for mobilic controls
+
+Revision ID: 575d1a347d04
+Revises: aa3c6ff8cdb7
+Create Date: 2023-09-05 10:33:30.873552
+
+"""
+from alembic import op
+from sqlalchemy.orm import Session
+
+from app.models.controller_control import ControllerControl
+
+# revision identifiers, used by Alembic.
+revision = "575d1a347d04"
+down_revision = "aa3c6ff8cdb7"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    conn = op.get_bind()
+    mobilic_controls = conn.execute(
+        """
+        SELECT id
+        FROM controller_control
+        WHERE control_type = 'mobilic'
+        AND observed_infractions IS NULL
+        """
+    )
+    session = Session(bind=conn)
+
+    for record in mobilic_controls:
+        control = (
+            session.query(ControllerControl).filter_by(id=record.id).first()
+        )
+        control.report_infractions()
+        session.commit()
+
+    session.close()
+
+
+def downgrade():
+    pass
