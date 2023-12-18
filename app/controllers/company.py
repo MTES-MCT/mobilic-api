5244ea91cb0770ef7a4d7974cf5a20a3f9c0ba5c
Gaspard Lonchampt <gaspard.lonchampt@beta.gouv.fr>
2023-12-18 14:56:53 +0100

    feat(brevoApi): add company and link with contact

--- app/controllers/company.py
+++ app/controllers/company.py
@@ -51,7 +51,13 @@ from app.models import Company, Employment
 from app.models.employment import (
     EmploymentRequestValidationStatus,
 )
-from app.helpers.brevo import brevo, CreateContactData, BrevoRequestError
+from app.helpers.brevo import (
+    brevo,
+    CreateContactData,
+    CreateCompanyData,
+    LinkCompanyContactData,
+    BrevoRequestError,
+)
 import sentry_sdk
 
 
@@ -193,13 +199,27 @@ def sign_up_company(usual_name, siren, sirets=[], send_email=True):
         db.session.add(admin_employment)
 
     try:
-        brevo.create_contact(
+        contact_id = brevo.create_contact(
             CreateContactData(
                 email=current_user.email,
                 admin_first_name=current_user.first_name,
                 admin_last_name=current_user.last_name,
                 company_name=company.usual_name,
                 siren=int(company.siren),
+            )
+        )
+
+        company_id = brevo.create_company(
+            CreateCompanyData(
+                company_name=company.usual_name,
+                siren=int(company.siren),
+            )
+        )
+
+        brevo.link_company_and_contact(
+            LinkCompanyContactData(
+                company_id=company_id,
+                contact_id=contact_id,
             )
         )
     except BrevoRequestError as e:
--- app/helpers/brevo.py
+++ app/helpers/brevo.py
@@ -1,6 +1,6 @@
-from __future__ import print_function
 from typing import NamedTuple
 
+import requests
 import sib_api_v3_sdk
 from sib_api_v3_sdk.rest import ApiException
 
@@ -23,13 +23,34 @@ class CreateContactData(NamedTuple):
     siren: int
 
 
+class CreateCompanyData(NamedTuple):
+    company_name: str
+    siren: int
+
+
+class LinkCompanyContactData(NamedTuple):
+    company_id: int
+    contact_id: int
+
+
 class BrevoApiClient:
+    BASE_URL = "https://api.brevo.com/v3/"
+
     def __init__(self, api_key):
         self._configuration = sib_api_v3_sdk.Configuration()
         self._configuration.api_key["api-key"] = api_key
-
+        self.api_key = api_key
         self._api_instance = sib_api_v3_sdk.ContactsApi(
             sib_api_v3_sdk.ApiClient(self._configuration)
+        )
+
+        self._session = requests.Session()
+        self._session.headers.update(
+            {
+                "api-key": api_key,
+                "accept": "application/json",
+                "content-type": "application/json",
+            }
         )
 
     def create_contact(self, data: CreateContactData):
@@ -46,7 +67,32 @@ class BrevoApiClient:
                 list_ids=[BREVO_COMPANY_SUBSCRIBE_LIST],
             )
             api_response = self._api_instance.create_contact(create_contact)
-            print(api_response)
+            return api_response.id
+        except ApiException as e:
+            raise BrevoRequestError(f"Request to Brevo API failed: {e}")
+
+    def create_company(self, data: CreateCompanyData):
+        try:
+            url = f"{self.BASE_URL}companies"
+            create_company_payload = {
+                "name": data.company_name,
+                "attributes": {
+                    "activation_mobilic": "Inscrite",
+                    "siren": data.siren,
+                    "owner": "Pathtech PATHTECH",
+                },
+            }
+            response = self._session.post(url, json=create_company_payload)
+            return response.json()["id"]
+        except ApiException as e:
+            raise BrevoRequestError(f"Request to Brevo API failed: {e}")
+
+    def link_company_and_contact(self, data: LinkCompanyContactData):
+        try:
+            url = f"{self.BASE_URL}companies/link-unlink/{data.company_id}"
+            payload = {"linkContactIds": [data.contact_id]}
+            response = self._session.patch(url, json=payload)
+            return response
         except ApiException as e:
             raise BrevoRequestError(f"Request to Brevo API failed: {e}")
 
