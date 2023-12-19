aa3495087bfa314c470a1c9668e2e05a8726dc0d
Tristan Gueguen <tristan.gueguen@gmail.com>
2023-12-19 13:01:06 +0100

    deleted at info in export PDF

--- app/helpers/pdf/mission_details.py
+++ app/helpers/pdf/mission_details.py
@@ -255,4 +255,5 @@ def generate_mission_details_pdf(
             max_reception_time,
         ),
         is_deleted=mission.is_deleted(),
+        deleted_at_text=f"Cette mission a été supprimée le {full_format_day(mission.deleted_at())}",
     )
