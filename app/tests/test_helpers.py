4c0406c412a1584364fbde23a6c70d4d1d472c6a
tristan-gueguen <77248807+tristan-gueguen@users.noreply.github.com>
2024-01-16 11:00:39 +0100

    Feat/holidays start end periods (#327)
    (feat)logging holidays on several days

--- app/tests/test_helpers.py
+++ app/tests/test_helpers.py
@@ -4,6 +4,7 @@ from unittest import TestCase
 from app.helpers.time import (
     get_uninterrupted_datetime_ranges,
     previous_month_period,
+    get_daily_periods,
 )
 
 
@@ -58,3 +59,43 @@ class TestHelper(TestCase):
         start, end = previous_month_period(date(2023, 3, 28))
         self.assertEqual(start, date(2023, 2, 1))
         self.assertEqual(end, date(2023, 2, 28))
+
+    def test_daily_periods(self):
+        start_date_time = datetime(2023, 2, 1, 8, 30)
+        end_date_time = datetime(2023, 2, 8, 17, 00)
+        periods = get_daily_periods(
+            start_date_time=start_date_time, end_date_time=end_date_time
+        )
+        self.assertEqual(8, len(periods))
+        self.assertEqual(periods[0][0].hour, 8)
+        self.assertEqual(periods[0][1].hour, 17)
+        self.assertEqual(periods[1][0].hour, 8)
+        self.assertEqual(periods[1][1].hour, 17)
+        self.assertEqual(periods[-1][0].day, 8)
+        self.assertEqual(periods[-1][1].day, 8)
+
+    def test_daily_periods_one_date(self):
+        start_date_time = datetime(2023, 2, 1, 8, 30)
+        end_date_time = datetime(2023, 2, 1, 17, 00)
+        periods = get_daily_periods(
+            start_date_time=start_date_time, end_date_time=end_date_time
+        )
+        self.assertEqual(1, len(periods))
+        self.assertEqual(periods[0][0].hour, 8)
+        self.assertEqual(periods[0][1].hour, 17)
+
+    def test_daily_periods_one_start_hour_after_end_hour(self):
+        start_date_time = datetime(2023, 2, 1, 17, 30)
+        end_date_time = datetime(2023, 2, 3, 8, 0)
+        periods = get_daily_periods(
+            start_date_time=start_date_time, end_date_time=end_date_time
+        )
+        self.assertEqual(2, len(periods))
+        self.assertEqual(periods[0][0].hour, 17)
+        self.assertEqual(periods[0][1].hour, 8)
+        self.assertEqual(periods[0][0].day, 1)
+        self.assertEqual(periods[0][1].day, 2)
+        self.assertEqual(periods[-1][0].hour, 17)
+        self.assertEqual(periods[-1][1].hour, 8)
+        self.assertEqual(periods[-1][0].day, 2)
+        self.assertEqual(periods[-1][1].day, 3)
