5bf224e9da8ec90e1e0fbfef4fbfe7eea67c06b2
Tristan Gueguen <tristan.gueguen@gmail.com>
2024-02-13 19:34:59 +0100

    use dataloaders for vehicles, expenditures, comments and validations in missions

--- app/data_access/mission.py
+++ app/data_access/mission.py
@@ -1,4 +1,5 @@
 import graphene
+from flask import g
 from graphene.types.generic import GenericScalar
 
 from app.helpers.controller_endpoint_utils import retrieve_max_reception_time
@@ -110,31 +111,57 @@ class MissionOutput(BaseSQLAlchemyObjectType):
 
     def resolve_expenditures(self, info, include_dismissed_expenditures=False):
         max_reception_time = retrieve_max_reception_time(info)
-        if max_reception_time:
-            return filter_out_future_events(
-                self.expenditures, max_reception_time
+        expenditures = g.dataloaders["expenditures_in_missions"].load(self.id)
+
+        def process_expenditures(expenditures):
+            if max_reception_time:
+                return filter_out_future_events(
+                    expenditures, max_reception_time
+                )
+            return (
+                expenditures
+                if include_dismissed_expenditures
+                else sorted(
+                    [e for e in expenditures if not e.is_dismissed],
+                    key=lambda e: e.reception_time,
+                )
             )
-        return (
-            self.expenditures
-            if include_dismissed_expenditures
-            else self.acknowledged_expenditures
+
+        return expenditures.then(
+            lambda expenditures: process_expenditures(expenditures)
         )
 
     def resolve_validations(self, info):
         max_reception_time = retrieve_max_reception_time(info)
-        if max_reception_time:
-            return filter_out_future_events(
-                self.validations, max_reception_time
-            )
-        return self.validations
+
+        def process_validations(validations):
+            if max_reception_time:
+                return filter_out_future_events(
+                    validations, max_reception_time
+                )
+            return validations
+
+        validations = g.dataloaders["validations_in_missions"].load(self.id)
+        return validations.then(
+            lambda validations: process_validations(validations)
+        )
 
     def resolve_comments(self, info):
         max_reception_time = retrieve_max_reception_time(info)
-        if max_reception_time:
-            return filter_out_future_events(
-                self.acknowledged_comments, max_reception_time
+        comments = g.dataloaders["comments_in_missions"].load(self.id)
+
+        def process_comments(comments):
+            acknowledged_comments = sorted(
+                [c for c in comments if not c.is_dismissed],
+                key=lambda c: c.reception_time,
             )
-        return self.acknowledged_comments
+            if max_reception_time:
+                return filter_out_future_events(
+                    acknowledged_comments, max_reception_time
+                )
+            return acknowledged_comments
+
+        return comments.then(lambda comments: process_comments(comments))
 
     def resolve_start_location(self, info):
         return self.start_location
@@ -153,6 +180,12 @@ class MissionOutput(BaseSQLAlchemyObjectType):
 
     def resolve_deleted_by(self, info):
         return self.deleted_by()
+
+    def resolve_vehicle(self, info):
+        if not self.vehicle_id:
+            return None
+
+        return g.dataloaders["vehicles"].load(self.vehicle_id)
 
 
 class MissionConnection(graphene.Connection):
