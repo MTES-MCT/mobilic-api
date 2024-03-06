fc44f3b5cef47d14f9a44482c03a3acde406a333
Gaspard Lonchampt <gaspard.lonchampt@beta.gouv.fr>
2024-03-06 21:46:44 +0100

    feat(controller): improve login error message

--- app/domain/controller.py
+++ app/domain/controller.py
@@ -1,20 +1,24 @@
 from flask import g
 from sqlalchemy import func
 
-from app import db
+from app import app, db
 from app.models.controller_user import ControllerUser
 from app.helpers.validation import clean_email_string
+from app.helpers.errors import AgentConnectAuthenticationError
 
 
 def create_controller_user(ac_info):
-    controller = ControllerUser(
-        agent_connect_id=ac_info.get("sub"),
-        first_name=ac_info.get("given_name"),
-        last_name=ac_info.get("usual_name"),
-        email=clean_email_string(ac_info.get("email")),
-        agent_connect_info=ac_info,
-        organizational_unit=ac_info.get("organizational_unit"),
-    )
+    try:
+        controller = ControllerUser(
+            agent_connect_id=ac_info.get("sub"),
+            first_name=ac_info.get("given_name"),
+            last_name=ac_info.get("usual_name"),
+            email=clean_email_string(ac_info.get("email")),
+            agent_connect_info=ac_info,
+            organizational_unit=ac_info.get("organizational_unit"),
+        )
+    except Exception as e:
+        raise AgentConnectAuthenticationError(app.logger.exception(e))
     db.session.add(controller)
     db.session.flush()
 
