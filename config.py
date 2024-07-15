1b7bd80e6bfba2d0f8d888dce4003c73a4f90134
Tristan Gueguen <tristan.gueguen@gmail.com>
2024-07-15 15:26:19 +0200

    deprecated option

--- config.py
+++ config.py
@@ -96,9 +96,6 @@ class Config:
     CELERY_BROKER_URL = os.environ.get(
         "CELERY_BROKER_URL", "redis://localhost:6379/0"
     )
-    CELERY_RESULT_BACKEND = os.environ.get(
-        "CELERY_RESULT_BACKEND", "redis://localhost:6379/0"
-    )
 
 
 class DevConfig(Config):
@@ -115,9 +112,6 @@ class DevConfig(Config):
     )
     CELERY_BROKER_URL = os.environ.get(
         "CELERY_BROKER_URL", "redis://localhost:6379/0"
-    )
-    CELERY_RESULT_BACKEND = os.environ.get(
-        "CELERY_RESULT_BACKEND", "redis://localhost:6379/0"
     )
 
 
