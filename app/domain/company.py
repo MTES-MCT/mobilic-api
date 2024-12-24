a2a38eeb17ad6eee09b82986ccc851c9474220a9
Gaspard Lonchampt <gaspard.lonchampt@beta.gouv.fr>
2024-12-24 13:42:27 +0100

    feat(invitation_email): adapt to mailjet maximum batch & restrict email invitations to a defined timeframe

--- app/domain/company.py
+++ app/domain/company.py
@@ -366,10 +366,11 @@ def find_admins_still_without_invitations(
 
 
 def find_employee_for_invitation(
-    first_employee_invitation_date, companies_to_exclude=None
+    first_employee_invitation_date,
+    max_start_date=None,
+    companies_to_exclude=None,
 ):
-
-    return (
+    query = (
         db.session.query(Employment)
         .options(joinedload(Employment.company))
         .join(Email, Email.employment_id == Employment.id)
@@ -393,6 +394,12 @@ def find_employee_for_invitation(
             )
             .correlate(Employment),
         )
-        .yield_per(50)
-        .all()
     )
+    if max_start_date:
+        query = query.filter(
+            Employment.creation_time
+            >= datetime.datetime.combine(
+                max_start_date, datetime.datetime.min.time()
+            )
+        )
+    return query.yield_per(100).all()
--- app/jobs/emails/bizdev/send_invitation_emails.py
+++ app/jobs/emails/bizdev/send_invitation_emails.py
@@ -1,10 +1,14 @@
+from app import app, db, mailer
 from datetime import timedelta
 
-from app import app, mailer
+from app.controllers.utils import atomic_transaction
+from app.helpers.mail import MailjetError
 from app.domain.company import find_employee_for_invitation
 from app.jobs import log_execution
 
 NB_DAYS_AGO = 7
+START_DATE = app.config["START_DATE_FOR_SCHEDULED_INVITATION"]
+MAILJET_BATCH_SEND_LIMIT = 50
 
 
 @log_execution
@@ -12,23 +16,53 @@ def send_invitation_emails(today):
     app.logger.info("Starting the invitation email sending process.")
 
     trigger_date = today - timedelta(days=NB_DAYS_AGO)
+    max_start_date = START_DATE
 
-    employments = find_employee_for_invitation(trigger_date)
+    if max_start_date:
+        app.logger.info(
+            f"Emails will only be sent for employments created between {max_start_date} and {trigger_date}."
+        )
+    else:
+        app.logger.info(
+            f"Emails will be sent for employments until {trigger_date}."
+        )
+
+    employments = find_employee_for_invitation(
+        trigger_date, max_start_date=max_start_date
+    )
     app.logger.info(
         f"Found {len(employments)} employments for sending invitations."
     )
 
     if employments:
-        try:
-            mailer.batch_send_employee_invites(
-                employments,
-                reminder=False,
-                disable_commit=False,
-                scheduled_reminder=True,
+        with atomic_transaction(commit_at_end=True):
+            messages = []
+            for cursor in range(0, len(employments), MAILJET_BATCH_SEND_LIMIT):
+                batch = employments[cursor : cursor + MAILJET_BATCH_SEND_LIMIT]
+                app.logger.info(
+                    f"Sending batch {cursor // MAILJET_BATCH_SEND_LIMIT + 1} with {len(batch)} employments."
+                )
+                messages.extend(
+                    mailer.batch_send_employee_invites(
+                        batch,
+                        reminder=False,
+                        disable_commit=True,
+                        scheduled_reminder=True,
+                    )
+                )
+
+            unsent_employments = []
+            for index, message in enumerate(messages):
+                if isinstance(message.response, MailjetError):
+                    unsent_employments.append(employments[index])
+                    db.session.delete(employments[index])
+                    app.logger.error(
+                        f"Error sending email to {employments[index].email}: {message.response}"
+                    )
+
+            app.logger.info(
+                f"Emails sent for {len(employments) - len(unsent_employments)} employments. "
+                f"{len(unsent_employments)} employments failed."
             )
-            app.logger.info(f"Emails sent for {len(employments)} employments.")
-        except Exception as e:
-            app.logger.error("Error during batch email sending.")
-            app.logger.exception(e)
 
     app.logger.info("Finished the invitation email sending process.")
