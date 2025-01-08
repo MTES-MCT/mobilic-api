da7d189b70c765d749a48bfd16b10820022dd292
Gaspard Lonchampt <gaspard.lonchampt@beta.gouv.fr>
2025-01-08 11:16:18 +0100

    feat(invitation_email): modify email template and start date

--- config.py
+++ config.py
@@ -112,7 +112,7 @@ class Config:
             os.environ.get("START_DATE_FOR_SCHEDULED_INVITATION"), "%Y-%m-%d"
         ).date()
         if os.environ.get("START_DATE_FOR_SCHEDULED_INVITATION")
-        else datetime(2024, 12, 31)
+        else datetime(2024, 12, 6)
     )
 
 
