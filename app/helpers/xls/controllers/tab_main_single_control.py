c55ccb42cc304adc11190dc16aae11b860d6b691
Tristan Gueguen <tristan.gueguen@gmail.com>
2025-01-13 14:32:31 +0100

    fix(control,excel): refacto write main sheet

--- app/helpers/xls/controllers/tab_main_single_control.py
+++ app/helpers/xls/controllers/tab_main_single_control.py
@@ -56,124 +56,135 @@ def write_main_sheet(wb, control, work_days_data=None):
     row_idx = write_header(wb, sheet, control)
 
     if control.control_type == ControlType.mobilic:
-        column_base_formats = write_tab_headers(
-            wb, sheet, row_idx, COLUMNS_MAIN
+        _write_main_sheet_mobilic(
+            wb=wb,
+            sheet=sheet,
+            row_idx=row_idx,
+            work_days_data=work_days_data,
+            control=control,
         )
-        row_idx += 1
-
-        recap_start_row = row_idx
-        has_one_bank_holiday = False
-        has_one_day_off = False
-        if work_days_data:
-            for wday in sorted(work_days_data, key=lambda wd: wd.day):
-                nb_infractions_for_day = len(
-                    [
-                        infraction
-                        for infraction in control.reported_infractions
-                        if datetime.strptime(
-                            infraction.get("date"), "%Y-%m-%d"
-                        ).date()
-                        == wday.day
-                    ]
-                )
-                wday.nb_infractions_for_day = nb_infractions_for_day
-                is_day_off = all([m.is_holiday() for m in wday.missions])
-                is_sunday_or_bank_holiday_ = is_sunday_or_bank_holiday(
-                    wday.day
-                )
-                bg_color = None
-                if is_sunday_or_bank_holiday_:
-                    has_one_bank_holiday = True
-                    bg_color = light_brown_hex
-                if is_day_off:
-                    has_one_day_off = True
-                    bg_color = blue_hex
-
-                col_idx = 0
-                write_cells(
-                    wb,
-                    sheet,
-                    column_base_formats,
-                    col_idx,
-                    row_idx,
-                    COLUMNS_MAIN,
-                    wday,
-                    with_border=True,
-                    bg_color=bg_color,
-                )
-                row_idx += 1
-                merge_cells_if_needed(
-                    wb,
-                    sheet,
-                    recap_start_row,
-                    row_idx,
-                    1,
-                    COLUMN_SIREN.lambda_value(wday),
-                    formats.get("merged_center"),
-                )
-                merge_cells_if_needed(
-                    wb,
-                    sheet,
-                    recap_start_row,
-                    row_idx,
-                    0,
-                    COLUMN_ENTREPRISE.lambda_value(wday),
-                    formats.get("merged_center"),
-                )
-
-        write_user_recap(
-            wb,
-            sheet,
-            COLUMNS_MAIN,
-            recap_start_row,
-            row_idx - 1,
-            control.user.display_name,
-        )
-
-        write_sheet_legend(
-            wb,
-            sheet,
-            has_bank_holiday=has_one_bank_holiday,
-            has_off_day=has_one_day_off,
-        )
-
     else:
-        write_tab_headers(wb, sheet, row_idx, COLUMNS_LIC_PAPIER)
-        row_idx += 1
-        infractions_by_date = {}
-        for infraction in control.reported_infractions:
-            infraction_date = datetime.strptime(
-                infraction.get("date"), "%Y-%m-%d"
-            ).date()
-            if infraction_date in infractions_by_date:
-                infractions_by_date[infraction_date].append(infraction)
-            else:
-                infractions_by_date[infraction_date] = [infraction]
+        _write_main_sheet_mobilic_lic_papier(
+            wb=wb, sheet=sheet, row_idx=row_idx, control=control
+        )
 
-        def _write_centered(_col_idx, _value, _additional_format={}):
-            sheet.write(
+
+def _write_main_sheet_mobilic(wb, sheet, row_idx, work_days_data, control):
+    column_base_formats = write_tab_headers(wb, sheet, row_idx, COLUMNS_MAIN)
+    row_idx += 1
+
+    recap_start_row = row_idx
+    has_one_bank_holiday = False
+    has_one_day_off = False
+    if work_days_data:
+        for wday in sorted(work_days_data, key=lambda wd: wd.day):
+            nb_infractions_for_day = len(
+                [
+                    infraction
+                    for infraction in control.reported_infractions
+                    if datetime.strptime(
+                        infraction.get("date"), "%Y-%m-%d"
+                    ).date()
+                    == wday.day
+                ]
+            )
+            wday.nb_infractions_for_day = nb_infractions_for_day
+            is_day_off = all([m.is_holiday() for m in wday.missions])
+            is_sunday_or_bank_holiday_ = is_sunday_or_bank_holiday(wday.day)
+            bg_color = None
+            if is_sunday_or_bank_holiday_:
+                has_one_bank_holiday = True
+                bg_color = light_brown_hex
+            if is_day_off:
+                has_one_day_off = True
+                bg_color = blue_hex
+
+            col_idx = 0
+            write_cells(
+                wb,
+                sheet,
+                column_base_formats,
+                col_idx,
                 row_idx,
-                _col_idx,
-                _value,
-                wb.add_format({**{"align": "center"}, **_additional_format}),
-            )
-
-        for infraction_date in sorted(infractions_by_date):
-            _write_centered(
-                _col_idx=0,
-                _value=infraction_date,
-                _additional_format={"num_format": "dd/mm/yyyy"},
-            )
-            _write_centered(
-                _col_idx=1, _value=len(infractions_by_date[infraction_date])
-            )
-            _write_centered(
-                _col_idx=2,
-                _value=", ".join(
-                    [
-                        i.get("sanction")
-                        for i in infractions_by_date[infraction_date]
-                    ]
-                ),
+                COLUMNS_MAIN,
+                wday,
+                with_border=True,
+                bg_color=bg_color,
             )
             row_idx += 1
+            merge_cells_if_needed(
+                wb,
+                sheet,
+                recap_start_row,
+                row_idx,
+                1,
+                COLUMN_SIREN.lambda_value(wday),
+                formats.get("merged_center"),
+            )
+            merge_cells_if_needed(
+                wb,
+                sheet,
+                recap_start_row,
+                row_idx,
+                0,
+                COLUMN_ENTREPRISE.lambda_value(wday),
+                formats.get("merged_center"),
+            )
+
+    write_user_recap(
+        wb,
+        sheet,
+        COLUMNS_MAIN,
+        recap_start_row,
+        row_idx - 1,
+        control.user.display_name,
+    )
+
+    write_sheet_legend(
+        wb,
+        sheet,
+        has_bank_holiday=has_one_bank_holiday,
+        has_off_day=has_one_day_off,
+    )
+
+
+def _write_main_sheet_mobilic_lic_papier(wb, sheet, row_idx, control):
+    write_tab_headers(wb, sheet, row_idx, COLUMNS_LIC_PAPIER)
+    row_idx += 1
+    infractions_by_date = {}
+    for infraction in control.reported_infractions:
+        infraction_date = datetime.strptime(
+            infraction.get("date"), "%Y-%m-%d"
+        ).date()
+        if infraction_date in infractions_by_date:
+            infractions_by_date[infraction_date].append(infraction)
+        else:
+            infractions_by_date[infraction_date] = [infraction]
+
+    def _write_centered(_col_idx, _value, _additional_format={}):
+        sheet.write(
+            row_idx,
+            _col_idx,
+            _value,
+            wb.add_format({**{"align": "center"}, **_additional_format}),
+        )
+
+    for infraction_date in sorted(infractions_by_date):
+        _write_centered(
+            _col_idx=0,
+            _value=infraction_date,
+            _additional_format={"num_format": "dd/mm/yyyy"},
+        )
+        _write_centered(
+            _col_idx=1, _value=len(infractions_by_date[infraction_date])
+        )
+        _write_centered(
+            _col_idx=2,
+            _value=", ".join(
+                [
+                    i.get("sanction")
+                    for i in infractions_by_date[infraction_date]
+                ]
+            ),
+        )
+        row_idx += 1
