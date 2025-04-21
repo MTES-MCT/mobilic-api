dda5eff4aa599404b06589b2335e493393a2ec0f
Gaspard Lonchampt <gaspard.lonchampt@beta.gouv.fr>
2025-04-21 21:03:30 +0200

    feat(database): add deletion target column and negative user ID sequence

--- migrations/versions/fe1e80dc69fe_add_negative_user_id_sequence_and_deletion_target.py
+++ migrations/versions/fe1e80dc69fe_add_negative_user_id_sequence_and_deletion_target.py
@@ -1,4 +1,4 @@
-"""add_negative_user_id_sequence
+"""add_negative_user_id_sequence_and_deletion_target
 
 Revision ID: fe1e80dc69fe
 Revises: 60f5c0616731
@@ -25,14 +25,12 @@ def upgrade():
         ALTER TABLE "user" DROP CONSTRAINT IF EXISTS useraccountstatus;
         """
     )
-
     op.execute(
         """
         ALTER TABLE "user" ADD CONSTRAINT useraccountstatus 
         CHECK (status IN ('active', 'blocked_bad_password', 'third_party_pending_approval', 'anonymized'));
         """
     )
-
     op.execute(
         """
         CREATE SEQUENCE IF NOT EXISTS negative_user_id_seq
@@ -43,21 +41,39 @@ def upgrade():
         CACHE 1;
         """
     )
-
     op.execute(
         """
         CREATE INDEX IF NOT EXISTS idx_user_status_anonymized ON "user" (status)
         WHERE status = 'anonymized';
         """
     )
-
     op.execute(
         """
         DROP TABLE anon_user;
         """
     )
+    op.add_column(
+        "temp_id_mapping",
+        sa.Column(
+            "deletion_target",
+            sa.Boolean(),
+            nullable=False,
+            server_default=sa.text("FALSE"),
+        ),
+    )
+    op.create_index(
+        op.f("ix_temp_id_mapping_deletion_target"),
+        "temp_id_mapping",
+        ["deletion_target"],
+        unique=False,
+    )
 
 
 def downgrade():
+    op.drop_index(
+        op.f("ix_temp_id_mapping_deletion_target"),
+        table_name="temp_id_mapping",
+    )
+    op.drop_column("temp_id_mapping", "deletion_target")
     op.execute("DROP INDEX IF EXISTS idx_user_status_anonymized;")
     op.execute("DROP SEQUENCE IF EXISTS negative_user_id_seq;")
