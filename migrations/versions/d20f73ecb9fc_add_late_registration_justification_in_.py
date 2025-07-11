945454f144ae24d465adddb7dd64e3fef241d368
Tristan Gueguen <tristan.gueguen@gmail.com>
2025-07-11 12:18:09 +0200

    feat(past-missions-justification): model and queries

---
+++ migrations/versions/d20f73ecb9fc_add_late_registration_justification_in_.py
@@ -0,0 +1,27 @@
+"""add late registration justification in mission
+
+Revision ID: d20f73ecb9fc
+Revises: 2403c7f1843d
+Create Date: 2025-07-11 10:34:41.244352
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "d20f73ecb9fc"
+down_revision = "2403c7f1843d"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column(
+        "mission",
+        sa.Column("past_registration_justification", sa.TEXT(), nullable=True),
+    )
+
+
+def downgrade():
+    op.drop_column("mission", "past_registration_justification")
