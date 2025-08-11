b20ee32e16fa173722cfdba3b72e873b420bbbb9
Tristan Gueguen <tristan.gueguen@gmail.com>
2025-08-11 10:48:20 +0200

    feat(certificate): migration: drop old certificate and create new one - basic model new certificate

--- app/models/company_certification.py
+++ app/models/company_certification.py
@@ -1,29 +1,31 @@
+from enum import IntEnum
+
 from sqlalchemy.orm import backref
 
 from app import db
 from app.models.base import BaseModel
 
 
+class CertificationLevel(IntEnum):
+    NO_CERTIFICATION = 0
+    BRONZE = 1
+    SILVER = 2
+    GOLD = 3
+    DIAMOND = 4
+
+
 class CompanyCertification(BaseModel):
     company_id = db.Column(
         db.Integer, db.ForeignKey("company.id"), index=True, nullable=False
     )
-    company = db.relationship("Company", backref=backref("certifications"))
+    company = db.relationship("Company", backref=backref("certifications_new"))
 
     attribution_date = db.Column(db.Date, nullable=False)
     expiration_date = db.Column(db.Date, nullable=False)
-    be_active = db.Column(db.Boolean, default=False, nullable=False)
-    be_compliant = db.Column(db.Boolean, default=False, nullable=False)
-    not_too_many_changes = db.Column(db.Boolean, default=False, nullable=False)
-    validate_regularly = db.Column(db.Boolean, default=False, nullable=False)
-    log_in_real_time = db.Column(db.Boolean, default=False, nullable=False)
+    log_in_real_time = db.Column(db.Float, default=0.0, nullable=False)
+    admin_changes = db.Column(db.Float, default=0.0, nullable=False)
+    compliancy = db.Column(db.Integer, default=0, nullable=False)
 
     @property
-    def certified(self):
-        return (
-            self.be_active
-            and self.be_compliant
-            and self.not_too_many_changes
-            and self.validate_regularly
-            and self.log_in_real_time
-        )
+    def certification_level(self):
+        return CertificationLevel.NO_CERTIFICATION
---
+++ migrations/versions/022d979043d0_certificate_rework_model.py
@@ -0,0 +1,48 @@
+"""certificate rework model
+
+Revision ID: 022d979043d0
+Revises: 68ed4db9ae15
+Create Date: 2025-07-30 10:12:10.850163
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "022d979043d0"
+down_revision = "68ed4db9ae15"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.drop_table("company_certification")
+    op.create_table(
+        "company_certification",
+        sa.Column("creation_time", sa.DateTime(), nullable=False),
+        sa.Column("company_id", sa.Integer(), nullable=False),
+        sa.Column("attribution_date", sa.Date(), nullable=False),
+        sa.Column("expiration_date", sa.Date(), nullable=False),
+        sa.Column("log_in_real_time", sa.Float(), nullable=False),
+        sa.Column("admin_changes", sa.Float(), nullable=False),
+        sa.Column("compliancy", sa.Integer(), nullable=False),
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["company_id"],
+            ["company.id"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_index(
+        op.f("ix_company_certification_company_id"),
+        "company_certification",
+        ["company_id"],
+        unique=False,
+    )
+
+
+def downgrade():
+    op.drop_table("company_certification_new")
+    pass
