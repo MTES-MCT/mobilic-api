f5b4af90e3b31da57a5703685895f85c6627c64f
Tristan Gueguen <tristan.gueguen@gmail.com>
2025-11-03 13:32:46 +0100

    opti(seed): refacto big scenario seed

---
+++ app/seed/scenarios/lot_of_missions.py
@@ -0,0 +1,129 @@
+import random
+
+from app import db
+from app.models import Business
+from app.models.expenditure import ExpenditureType
+from app.seed import (
+    CompanyFactory,
+    UserFactory,
+    EmploymentFactory,
+)
+from app.seed.helpers import (
+    add_employee,
+    get_time,
+    DEFAULT_PASSWORD,
+    create_vehicle,
+    create_address,
+    log_and_validate_mission,
+)
+
+ADMIN_EMAIL = "test.charge@admin.com"
+NB_EMPLOYEES = 2
+NB_HISTORY_DELETED = 4
+# most recent day: nobody validates
+# day before: only employee validates
+# before: two validations
+NB_HISTORY_REGULAR = 5
+NB_VEHICLES = 20
+NB_ADDRESSES = 20
+
+
+def run_scenario_lot_of_missions():
+    business = Business.query.first()
+    company = CompanyFactory.create(
+        usual_name=f"Test Charge",
+        siren=f"1000001",
+        number_workers=NB_EMPLOYEES + 1,
+        business=business,
+    )
+    admin = UserFactory.create(
+        email=ADMIN_EMAIL,
+        password=DEFAULT_PASSWORD,
+        first_name="Prenom",
+        last_name="Nom",
+    )
+    EmploymentFactory.create(
+        company=company, submitter=admin, user=admin, has_admin_rights=True
+    )
+    vehicles = [create_vehicle(company=company) for _ in range(NB_VEHICLES)]
+    db.session.commit()
+
+    employees = [
+        add_employee(
+            company=company,
+            admin=admin,
+        )
+        for _ in range(NB_EMPLOYEES)
+    ]
+    db.session.commit()
+
+    addresses = [create_address(company=company) for _ in range(NB_ADDRESSES)]
+    db.session.commit()
+
+    deleted_missions = []
+    for employee in employees:
+
+        for nb_days_ago in range(NB_HISTORY_DELETED):
+            mission = log_and_validate_mission(
+                f"Mission supprimée {nb_days_ago + 1}",
+                company=company,
+                employee=employee,
+                vehicle=random.choice(vehicles),
+                address=random.choice(addresses),
+                add_location_entry=True,
+                work_periods=[
+                    [
+                        get_time(how_many_days_ago=nb_days_ago + 1, hour=14),
+                        get_time(how_many_days_ago=nb_days_ago + 1, hour=15),
+                    ]
+                ],
+                employee_comment="Commentaire du salarié",
+                employee_expenditure=random.choice(list(ExpenditureType)),
+            )
+
+            deleted_missions.append(mission)
+
+    db.session.commit()
+    from app.tests.helpers import make_authenticated_request, ApiRequests
+
+    # Admin cancels missions
+    for deleted_mission in deleted_missions:
+        make_authenticated_request(
+            time=get_time(how_many_days_ago=1, hour=17),
+            submitter_id=admin.id,
+            query=ApiRequests.cancel_mission,
+            variables=dict(
+                mission_id=deleted_mission.id,
+                user_id=deleted_mission.submitter_id,
+            ),
+        )
+    db.session.commit()
+
+    # Regular missions
+    for employee in employees:
+        for nb_days_ago in range(NB_HISTORY_REGULAR):
+            employee_validates = nb_days_ago > 0
+            admin_validates = nb_days_ago > 1
+            log_and_validate_mission(
+                mission_name=f"Mission {nb_days_ago}",
+                company=company,
+                employee=employee,
+                vehicle=random.choice(vehicles),
+                address=random.choice(addresses),
+                add_location_entry=True,
+                work_periods=[
+                    [
+                        get_time(how_many_days_ago=nb_days_ago + 1, hour=6),
+                        get_time(how_many_days_ago=nb_days_ago + 1, hour=10),
+                    ],
+                    [
+                        get_time(how_many_days_ago=nb_days_ago + 1, hour=14),
+                        get_time(how_many_days_ago=nb_days_ago + 1, hour=18),
+                    ],
+                ],
+                employee_expenditure=random.choice(list(ExpenditureType)),
+                validate=employee_validates,
+                admin_validating=admin if admin_validates else None,
+                employee_comment="Commentaire du salarié",
+            )
+            db.session.commit()
