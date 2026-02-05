f244b045d3d61188b3fe930128684d566f478582
tristan-gueguen <tristan.gueguen@gmail.com>
2026-02-05 18:25:33 +0100

    feat(new-bdc): first page

--- app/helpers/pdf/control_bulletin.py
+++ app/helpers/pdf/control_bulletin.py
@@ -1,7 +1,22 @@
+from app.domain.business import get_businesses_display_name
+from app.domain.regulations import get_default_business
 from app.helpers.pdf import generate_pdf_from_template
+from app.models.controller_control import ControlType
 
 
 def generate_control_bulletin_pdf(control):
+
+    is_control_mobilic = control.control_type == ControlType.mobilic
+
+    if is_control_mobilic:
+        business_ids = list(
+            control.control_bulletin.get("employments_business_types").values()
+        )
+        business_types = get_businesses_display_name(business_ids=business_ids)
+    else:
+        business_id = control.control_bulletin.get("business_id", None)
+        business = get_default_business(business_id=business_id)
+        business_types = business.display_name
 
     return generate_pdf_from_template(
         "control_bulletin.html",
@@ -10,11 +25,16 @@ def generate_control_bulletin_pdf(control):
             control.controller_user, "pretty_organizational_unit", None
         ),
         control_time=control.creation_time,
+        control_end_time=control.control_bulletin_update_time,
         control_date=control.creation_time,
-        control_location=f"{control.control_bulletin.get('location_lieu')}, {control.control_bulletin.get('location_commune')}",
-        controlled_employee=f"{control.user_last_name} {control.user_first_name}",
-        controlled_company=f"{control.company_name} - {control.control_bulletin.get('siren')}",
-        controller_company_address=f"{control.control_bulletin.get('company_address')}",
+        # control_location=f"{control.control_bulletin.get('location_lieu')}, {control.control_bulletin.get('location_commune')}",
+        controlled_employee_first_name=control.user_first_name,
+        controlled_employee_last_name=control.user_last_name,
+        controlled_employee_birth_date=control.control_bulletin.get('user_birth_date'),
+        controlled_employee_nationality=control.control_bulletin.get('user_nationality'),
+        controlled_company_siren=control.control_bulletin.get('siren'),
+        controlled_company_name=control.company_name,
+        controlled_company_address=f"{control.control_bulletin.get('company_address')}",
         transport_type=control.control_bulletin.get("transport_type"),
         articles_nature=control.control_bulletin.get("articles_nature"),
         license_number=control.control_bulletin.get("license_number"),
@@ -36,7 +56,9 @@ def generate_control_bulletin_pdf(control):
             if control.controller_user
             else "Contrôleur inconnu"
         ),
+        controller_greco_id=control.controller_user.greco_id,
         infraction_labels=control.reported_infractions_labels,
         history_start_date=control.history_start_date,
         history_end_date=control.history_end_date,
+        business_types=business_types
     )
