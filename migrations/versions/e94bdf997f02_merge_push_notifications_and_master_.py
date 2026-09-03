5e4251c8f5b9e5e5d6fdcf3aa73bdfda0aa67c52
MrJimmyChevallier <103670713+MrJimmyChevallier@users.noreply.github.com>
2026-09-03 11:33:57 +0200

    fix(migrations): merge push notifications and master migration heads

---
+++ migrations/versions/e94bdf997f02_merge_push_notifications_and_master_.py
@@ -0,0 +1,24 @@
+"""merge push notifications and master migration heads
+
+Revision ID: e94bdf997f02
+Revises: 74b8a795d8f5
+Create Date: 2026-09-03 09:32:28.733783
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e94bdf997f02'
+down_revision = '74b8a795d8f5'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
