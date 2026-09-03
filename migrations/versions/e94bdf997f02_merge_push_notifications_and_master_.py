4e0d4400d850250a1c9a36ebccd1bfc5c3236a41
MrJimmyChevallier <103670713+MrJimmyChevallier@users.noreply.github.com>
2026-09-03 12:00:51 +0200

    fix(migrations): fix merge migration to include both heads

--- migrations/versions/e94bdf997f02_merge_push_notifications_and_master_.py
+++ migrations/versions/e94bdf997f02_merge_push_notifications_and_master_.py
@@ -1,7 +1,7 @@
 """merge push notifications and master migration heads
 
 Revision ID: e94bdf997f02
-Revises: 74b8a795d8f5
+Revises: 74b8a795d8f5, 0da0a33704b0
 Create Date: 2026-09-03 09:32:28.733783
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'e94bdf997f02'
-down_revision = '74b8a795d8f5'
+down_revision = ('74b8a795d8f5', '0da0a33704b0')
 branch_labels = None
 depends_on = None
 
